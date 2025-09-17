561b1c9403a1d7cdb08c73df11d3a5c5a8c0d771
Pi-Eatery <kam3014@proton.me>
2025-09-17 16:30:35 +0000

    Removed immediate print statements after finding the List element. Formatted the output to be a singular string that is easier to parse with human eyes

--- bin/BasicWebScrape.py
+++ bin/BasicWebScrape.py
@@ -9,7 +9,6 @@ def openTheMail():
     Address = Envelope.select('h1#product-title')
     City = Address[0]
     Street = City.get_text(strip=True)
-    print(f"The name of the product is: {Street}")
 
     ReturnAddress = Envelope.select('span.currency-symbol')
     Stamp = Envelope.select('span.price-value')
@@ -17,13 +16,12 @@ def openTheMail():
     CollectorStamp = Stamp[0]
     PostalCode = CountryOfOrigin.get_text(strip=True)
     RareFactor = CollectorStamp.get_text(strip=True)
-    print(f"The price of the product is: {PostalCode}{RareFactor}")
 
     Phone = Envelope.find_all('li')
     for Name in Phone:
         if "SKU:" in Name.get_text():
-            print(Name.get_text())
-        exit
+            MailManName = Name.get_text()
+            break
 
     PostKeys = Envelope.find_all(string=lambda text: isinstance(text, Comment))
     for Key in PostKeys:
@@ -31,6 +29,8 @@ def openTheMail():
             print(f"API KEY FOUND EXPOSED\nRESOLVE SECURITY VULNERABILITY IMMEDIATELY")
         else:
             print(f"Comment found...\nRevealing now...\n{Key.strip()} (This comment is this type: {type(Key)})")
+
+    print(f"Found {Street} ({MailManName}) for {PostalCode}{RareFactor}")
 
 if __name__ == '__main__':
     openTheMail()    
