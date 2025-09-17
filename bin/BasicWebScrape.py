c22c3c414d2de35f59f14a0758325b8db30eb0cc
Pi-Eatery <kam3014@proton.me>
2025-09-17 14:13:55 +0000

    Finished debugging the code.\nI learned about lambda functions to find specific strings within a comment in an HTML file.\n The code now does the followingReads the HTML file in its code\nPrints any comments it finds that is not just whitespace while noting the type of header it belongs to\nIt then prints out the strings of selected elements within the HTML file in a clean format\nThen it outputs those variables into a string to show the user what it found in an easy to read format.

--- bin/BasicWebScrape.py
+++ bin/BasicWebScrape.py
@@ -8,7 +8,6 @@ def openTheMail():
     counter = 0
     
     Route = Envelope.find_all()
-    PostKeys = Envelope.find_all()
         
     Address = Envelope.select('h1#product-title')
     City = Address[0]
@@ -22,24 +21,20 @@ def openTheMail():
     CollectorStamp = Stamp[0]
     RareFactor = CollectorStamp.get_text(strip=True)
 
-    SecretMessage = ""
+    PostKeys = Envelope.find_all(string=lambda text: isinstance(text, Comment))
+    keyCounter = 0
 
-    MasterKey = Envelope.find_all(isinstance('text', Comment))
+    for Key in PostKeys:
+        if Key.strip():
+            print(f"Comment found...\nRevealing now...\n{Key.strip()} (It is of the type: {type(Key)})")
+            keyCounter + 1 == keyCounter
+            PromisingKeys = [Key.strip() for Key in PostKeys if "API" in Key]
 
-    for counter in PostKeys:
-        MasterKeyCandidate = Envelope.find_all('div'),(counter)
-        if MasterKey == Envelope.find_all(isinstance('text', Comment)):
-            Engraving = MasterKey
-            print(f"After {counter} keys I finally found it! The engraved master key!\nThe engraving on it says {Engraving}")
-        else:
-            Engraving = "Made you look!" 
-            counter + 1 == counter
-            if counter > 5:
-                print(f"{counter} keys checked now I've seen a few engraved messages.\nOne of these keys might be the master key...\n Hah! {Engraving}")
+    MasterKey = PromisingKeys
+    SecretMessage = MasterKey[0]
 
  #   print(f"\nThis is what the news had in the Classified today: {Route}")
-    print(f"The {Address} costs {CountryOfOrigin}{CollectorStamp}! I can't believe the scammers nowadays")
-    return
+    print(f"The {Street} costs {PostalCode}{RareFactor}! I can't believe the scammers nowadays\n What is this? {SecretMessage}")
 
 if __name__ == '__main__':
     openTheMail()    
