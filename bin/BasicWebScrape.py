fcadc844037daca4234d55345264b2a22106cfdc
Pi-Eatery <kam3014@proton.me>
2025-09-17 13:31:10 +0000

    Made some general improvements. Now fixing bugs

--- bin/BasicWebScrape.py
+++ bin/BasicWebScrape.py
@@ -2,32 +2,44 @@ import lxml
 from bs4 import BeautifulSoup, Comment
 
 with open('../example.html', 'r') as Goodies:
-    Envelope = BeautifulSoup(
-        Goodies, 
-        'html.parser'
-    )
+    Envelope = BeautifulSoup(Goodies, 'html.parser')
 
 def openTheMail():
     counter = 0
-    Address = Envelope.select('h1#product-title')
-    Stamp = Envelope.select('span.price-value')
-    ReturnAddress = Envelope.select('span.currency-symbol')
-    CollectorStamp = Stamp
-    CountryOfOrigin = ReturnAddress
-    PostKeys = Envelope.find_all('div')
-    SecretMessage = ""
-    while counter != enumerate(PostKeys): 
-        MasterKey = Envelope.select('div')[counter]
-        counter += counter
-        return MasterKey
     
-    Engraving = MasterKey.get_text()
-    if Engraving.includes("*API*"):
-        SecretMessage = Engraving
+    Route = Envelope.find_all()
+    PostKeys = Envelope.find_all()
+        
+    Address = Envelope.select('h1#product-title')
+    City = Address[0]
+    Street = City.get_text(strip=True)
 
-    print(f"The {Address} costs {CountryOfOrigin}{CollectorStamp}!")
-    if SecretMessage != "":
-        print(f"\nDon't tell the Mailman but I have their keys :) \nLook at what's engraved {SecretMessage}")
+    ReturnAddress = Envelope.select('span.currency-symbol')
+    CountryOfOrigin = ReturnAddress[0]
+    PostalCode = CountryOfOrigin.get_text(strip=True)
+
+    Stamp = Envelope.select('span.price-value')
+    CollectorStamp = Stamp[0]
+    RareFactor = CollectorStamp.get_text(strip=True)
+
+    SecretMessage = ""
+
+    MasterKey = Envelope.find_all(isinstance('text', Comment))
+
+    for counter in PostKeys:
+        MasterKeyCandidate = Envelope.find_all('div'),(counter)
+        if MasterKey == Envelope.find_all(isinstance('text', Comment)):
+            Engraving = MasterKey
+            print(f"After {counter} keys I finally found it! The engraved master key!\nThe engraving on it says {Engraving}")
+        else:
+            Engraving = "Made you look!" 
+            counter + 1 == counter
+            if counter > 5:
+                print(f"{counter} keys checked now I've seen a few engraved messages.\nOne of these keys might be the master key...\n Hah! {Engraving}")
+
+ #   print(f"\nThis is what the news had in the Classified today: {Route}")
+    print(f"The {Address} costs {CountryOfOrigin}{CollectorStamp}! I can't believe the scammers nowadays")
+    return
 
 if __name__ == '__main__':
     openTheMail()    
