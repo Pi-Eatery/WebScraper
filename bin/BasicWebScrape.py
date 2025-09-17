faba819c064b89c533f033a5426c9270aedd1453
Pi-Eatery <kam3014@proton.me>
2025-09-17 16:19:47 +0000

    Refactored to make less brittle and more efficient

--- bin/BasicWebScrape.py
+++ bin/BasicWebScrape.py
@@ -6,38 +6,31 @@ with open('../example.html', 'r') as Goodies:
 
 def openTheMail():
 
-    Route = Envelope.find_all()
-        
     Address = Envelope.select('h1#product-title')
     City = Address[0]
     Street = City.get_text(strip=True)
+    print(f"The name of the product is: {Street}")
 
     ReturnAddress = Envelope.select('span.currency-symbol')
-    CountryOfOrigin = ReturnAddress[0]
-    PostalCode = CountryOfOrigin.get_text(strip=True)
-
     Stamp = Envelope.select('span.price-value')
+    CountryOfOrigin = ReturnAddress[0]
     CollectorStamp = Stamp[0]
+    PostalCode = CountryOfOrigin.get_text(strip=True)
     RareFactor = CollectorStamp.get_text(strip=True)
+    print(f"The price of the product is: {PostalCode}{RareFactor}")
 
     Phone = Envelope.find_all('li')
-    MailManNum = Phone[2]
-    MailManName = MailManNum.get_text(strip=True)
-    print(MailManName)
+    for Name in Phone:
+        if "SKU:" in Name.get_text():
+            print(Name.get_text())
+        exit
 
     PostKeys = Envelope.find_all(string=lambda text: isinstance(text, Comment))
-
     for Key in PostKeys:
-        PromisingKeys = [Key.strip() for Key in PostKeys if "API" in Key]
-        if Key.strip() == PromisingKeys[0]:
+        if "API" in Key:
             print(f"API KEY FOUND EXPOSED\nRESOLVE SECURITY VULNERABILITY IMMEDIATELY")
         else:
             print(f"Comment found...\nRevealing now...\n{Key.strip()} (This comment is this type: {type(Key)})")
-
-    MasterKey = PromisingKeys
-    SecretMessage = MasterKey[0]
-
-    print(f"The {Street}({MailManName}) cost me {PostalCode}{RareFactor}! I can't believe the scammers nowadays")
 
 if __name__ == '__main__':
     openTheMail()    
