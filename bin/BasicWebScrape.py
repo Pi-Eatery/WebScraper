abbdaf0017309224f7ae3bff8dcf1b554f175c62
Pi-Eatery <kam3014@proton.me>
2025-09-17 14:32:11 +0000

    Removed the keyCounter variable, as it was not working the way I wanted/it was unecessary

--- bin/BasicWebScrape.py
+++ bin/BasicWebScrape.py
@@ -5,8 +5,7 @@ with open('../example.html', 'r') as Goodies:
     Envelope = BeautifulSoup(Goodies, 'html.parser')
 
 def openTheMail():
-    counter = 0
-    
+
     Route = Envelope.find_all()
         
     Address = Envelope.select('h1#product-title')
@@ -22,19 +21,17 @@ def openTheMail():
     RareFactor = CollectorStamp.get_text(strip=True)
 
     PostKeys = Envelope.find_all(string=lambda text: isinstance(text, Comment))
-    keyCounter = 0
 
     for Key in PostKeys:
         if Key.strip():
             print(f"Comment found...\nRevealing now...\n{Key.strip()} (It is of the type: {type(Key)})")
-            keyCounter + 1 == keyCounter
             PromisingKeys = [Key.strip() for Key in PostKeys if "API" in Key]
 
     MasterKey = PromisingKeys
     SecretMessage = MasterKey[0]
 
  #   print(f"\nThis is what the news had in the Classified today: {Route}")
-    print(f"The {Street} costs {PostalCode}{RareFactor}! I can't believe the scammers nowadays\n What is this? {SecretMessage}")
+    print(f"The {Street} costs {PostalCode}{RareFactor}! I can't believe the scammers nowadays\nWhat is this? {SecretMessage}")
 
 if __name__ == '__main__':
     openTheMail()    
